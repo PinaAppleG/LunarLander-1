02b4ea6e2151bfc35639d3793bcf207c37afee2b
stillsj1 <stills.s.j@gmail.com>
2017-03-14 09:19:59 -0700

    Push project to Github.

--- lunar_lander/nn_learning_agent.py
+++ lunar_lander/nn_learning_agent.py
@@ -5,77 +5,92 @@ from collections import deque
 import numpy as np
 from sklearn.neural_network import MLPRegressor
 from sklearn.preprocessing import OneHotEncoder
-from sklearn.preprocessing import StandardScaler
 
 
 class LearningAgent:
-    """
-    Learning agent intended to solve the lunar lander environment from OpenAI.
-    The agent uses on policy, epsilon greedy Q-Learning backed by a neural
-    network with experience replay.
-    """
+    def __init__(self, replay_size=100000, batch_size=128, gamma=0.995,
+                 epsilon=1, min_epsilon=0.05, epsilon_decay_rate=0.995,
+                 training_rate=1, training_decay_rate=1, min_training_rate=0,
+                 n_explorations=25):
+        """
+        Learning agent intended to solve the lunar lander environment from
+        OpenAI. The agent uses on policy, epsilon greedy Q-Learning backed by a
+        neural network with experience replay.
 
-    def __init__(self):
+        :param replay_size: replay memory size (default: 100000)
+        :param batch_size:  batch training size (default: 128)
+        :param gamma: discount factor (default: 0.995)
+        :param epsilon: epsilon exploration (default: 1)
+        :param min_epsilon: minimum epsilon (default: 0.05)
+        :param epsilon_decay_rate: rate at which epsilon decays (default: 0.995)
+        :param training_rate: training rate (default: 1)
+        :param training_decay_rate: rate at which training decays (default: 1)
+        :param min_training_rate: minimum training rate (default: 0)
+        :param n_explorations: number of episodes to explore (default: 25)
+        """
         # Neural Network
         self.nn = MLPRegressor(warm_start=True)
         self.trained = False
 
         # Mini batch settings
-        self.replay_size = 20000
-        self.replay_data = deque(maxlen=self.replay_size)
-        self.batch_size = 50
+        self.replay_size = replay_size
+        self.replay_memory = deque(maxlen=self.replay_size)
+        self.batch_size = batch_size
 
         # Hyper parameters
-        self.gamma = 0.995
-        self.eps = 1
-        self.min_eps = 0.05
-        self.eps_decay = .995
+        self.gamma = gamma
+        self.epsilon = epsilon
+        self.min_epsilon = min_epsilon
+        self.epsilon_decay_rate = epsilon_decay_rate
 
-        # Queue of rewards received
-        self.rewards = deque(maxlen=100)
+        # Training frequency settings
+        self.training_rate = training_rate
+        self.training_decay_rate = training_decay_rate
+        self.min_training_rate = min_training_rate
+
+        # Number of episodes to purely explore.
+        self.n_explorations = n_explorations
 
         # One hot encode actions
-        enc = OneHotEncoder()
         self.unencoded_actions = [0, 1, 2, 3]
+        enc = OneHotEncoder()
         self.actions = enc.fit_transform(
             [[x] for x in self.unencoded_actions]).toarray()
 
-        # Data normalizer
-        self.scalar = StandardScaler()
-        self.scalar_fitted = False
-
-    def get_action(self, observation):
+    def act(self, observation):
         """
         Preforms epsilon greedy exploration to determine what action the agent
         should take. If the neural network has yet to be trained, a action
         is randomly sampled.
+
         :param observation: observation describing the current state of the
         OpenAI Gym environment.
         :return: valid lunar lander action
         """
-        # If the neural network is untrained sample a random action.
+        # If the neural network is untrained(exploring) sample a random action.
         if not self.trained:
             return random.choice(self.unencoded_actions)
 
         # Perform epsilon greedy exploration
-        elif random.random() > self.eps:
+        elif self.epsilon < random.random():
+            return random.choice(self.unencoded_actions)
+        else:
             inputs = [np.concatenate((observation, action)) for action in
                       self.actions]
-            inputs = self.scalar.transform(inputs)
             outputs = self.nn.predict(inputs)
             action, _ = max(enumerate(outputs), key=operator.itemgetter(1))
             return action
-        else:
-            return random.choice(self.unencoded_actions)
 
     def store_in_replay(self, observation, action, reward, new_observation,
                         done):
         """
         Stores needed information for learning.
+
         :param observation: observation(state) before the action was taken.
         :param action: action that was taken.
         :param reward: reward received after taking the action.
-        :param new_observation: observation(state) after the action was taken.
+        :param new_observation: new observation(state) after the action was
+        taken.
         :param done: boolean to indicate if the episode has completed.
         :return: None
         """
@@ -84,44 +99,62 @@ class LearningAgent:
         # rewards.
         if done or not self.trained:
             y = reward
-            self.eps = max(self.min_eps, self.eps * self.eps_decay)
+            self.epsilon = max(self.min_epsilon,
+                               self.epsilon * self.epsilon_decay_rate)
         else:
             inputs = [np.concatenate((new_observation, a)) for a in
                       self.actions]
-            inputs = self.scalar.transform(inputs)
             outputs = self.nn.predict(inputs)
             _, value = max(enumerate(outputs), key=operator.itemgetter(1))
             y = reward + self.gamma * value
 
-        self.replay_data.append(
+        # Append observation action pair and y value to memory.
+        self.replay_memory.append(
             (np.concatenate((observation, self.actions[action])), y))
 
-    def train(self):
+        # If episode over, decrement exploring count.
+        if done:
+            self.n_explorations = max(0, self.n_explorations - 1)
+
+        # If not exploring, train if training_rate is met.
+        if self.n_explorations == 0 and random.random() < self.training_rate:
+            self.__train()
+            self.training_rate = max(self.min_training_rate,
+                                     self.training_rate *
+                                     self.training_decay_rate)
+
+    def __train(self):
         """
-        Sample a mini-batch from the experience replay and training the neural
+        Sample a mini-batch from the experience replay and train the neural
         network.
+
         :return: None
         """
-        samples = random.sample(self.replay_data, self.batch_size)
+        samples = random.sample(self.replay_memory, self.batch_size)
         x = [sample[0] for sample in samples]
         y = [sample[1] for sample in samples]
-
-        # Fit the data normalizer if has yet to be fitted.
-        if not self.scalar_fitted:
-            data = [d[0] for d in samples]
-            self.scalar.fit(data)
-            self.scalar_fitted = True
-
-        x = self.scalar.transform(x)
 
         self.nn.fit(x, y)
         self.trained = True
 
-    def no_eps(self):
+    def set_attributes(self, attr_settings):
         """
-        Set epsilon to zero, the agent will now always attempt to maximize its
-        reward, rather than performing epsilon greedy exploration.
+        Takes a dictionary of attribute settings.
+        :param attr_settings: dictionary of attribute settings.
+        example: {'training_rate': 0.5, training_decay_rate: 0}
         :return: None
         """
-        self.eps = 0
-        self.min_eps = 0
+        for key, value in attr_settings.items():
+            setattr(self, key, value)
+
+    def stop_training(self):
+        """
+        Stop training by setting training rate to 0, also set epsilon to 0 to
+        prevent any exploration.
+
+        :return: None
+        """
+        self.epsilon = 0
+        self.min_epsilon = 0
+        self.min_training_rate = 0
+        self.training_rate = 0
--- main.py
+++ main.py
@@ -5,14 +5,14 @@ import gym
 from lunar_lander.nn_learning_agent import LearningAgent
 
 # Gym settings
-max_episodes = 25000
+max_episodes = 50000
 
 # Episode rewards queue
 rewards = deque(maxlen=100)
 
 # Recording settings
 record = True
-recording_file = '/tmp/lander_test_123'
+recording_file = '/tmp/lunar_lander'
 key = 'sk_WqHacrBQcaSmyTwOqpLtg'
 
 # Init Lunar Lander Gym
@@ -24,21 +24,14 @@ env.reset()
 # Agent
 agent = LearningAgent()
 
-# Exploring settings
-action_time_step_range = 10
-exploring = True
-exploring_episodes = 25
-
-# Freeze training after every action, and instead train after every episode
-# to prevent over fitting once the average reward over the last 100 episodes
-# exceeds the freeze_at value.
-freeze = False
-freeze_at = 100
-
 # Agent is consider trained and should stop additional learning once the average
 # reward over the last 100 episodes exceeds the stop_at value.
-stop = False
-stop_at = 185
+stop_at = 200
+
+# Agent will train less frequently once performing well to help avoid over
+# fitting, aka freeze.
+freeze_at = 100
+frozen_training_rate = 0.1
 
 # Begin running gym episodes
 for i_episode in range(max_episodes):
@@ -49,58 +42,39 @@ for i_episode in range(max_episodes):
     episode_reward = 0
     time_step = 0
 
-    # Turn off pure exploration mode once threshold is reached
-    if i_episode > exploring_episodes:
-        exploring = False
-
-    # If not exploring, a new action should be chosen at every time step
-    if not exploring:
-        action_time_step_range = 1
-
     # Let agent complete the episode, force end the episode if it last longer
-    # than 1000 time steps
-    while not done and time_step < 1000:
-
+    # than 100000 time steps (if it gets stuck)
+    while not done and time_step < 100000:
+        # Backup old observation
         old_observation = observation
 
-        action = agent.get_action(old_observation)
+        # Select action to take
+        action = agent.act(old_observation)
 
-        # Repeat selected action for action_time_step_range time steps
-        for i in range(action_time_step_range):
-            observation, reward, done, info = env.step(action)
-            time_step += 1
-            episode_reward += reward
-            agent.store_in_replay(old_observation, action, reward, observation,
-                                  done)
-            if done:
-                break
+        # Take action
+        observation, reward, done, info = env.step(action)
+        time_step += 1
+        episode_reward += reward
+        agent.store_in_replay(old_observation, action, reward, observation,
+                              done)
 
-        # If not in freeze or exploring mode, agent should train each time step
-        if not exploring and not freeze:
-            agent.train()
-
+    # Append episode reward, take running average
     rewards.append(episode_reward)
     avg_reward = sum(rewards) / len(rewards)
 
-    # Enable freeze mode is threshold has been reached
     if avg_reward >= freeze_at:
-        freeze = True
+        agent.set_attributes({'training_rate': frozen_training_rate})
 
     # Stop all training, and set epsilon to 0 if threshold has been reached
     if avg_reward >= stop_at:
-        agent.no_eps()
-        stop = True
-
-    # If per time step training is frozen, train after the episode
-    if freeze and not stop:
-        agent.train()
+        agent.stop_training()
 
     # Log some stats to standard out
     if i_episode > 100:
-        STATS_FMT = "Ep: {}, Avg: {}, Cur: {}, Freeze: {}, Stop: {}"
-        print(STATS_FMT.format(i_episode, avg_reward, episode_reward, freeze,
-                               stop))
+        STATS_FMT = "Ep: {}, Avg: {}, Cur: {}"
+        print(STATS_FMT.format(i_episode, avg_reward, episode_reward))
 
+# Upload recording to OpenAI
 if record:
     env.monitor.close()
     gym.upload(recording_file, api_key=key)
